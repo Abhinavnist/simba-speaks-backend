319c0a9d85714bd751d8a40ff426424de90669fe
abhinavnist <abhinavpatna55@gmail.com>
2023-10-03 20:41:31 +0530

    database connection

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const mongoose = require("mongoose")
 require("dotenv").config()
 const app = express()
 
@@ -6,6 +7,16 @@ app.get("/", function (req, res) {
   res.send("Hello World")
 })
 
-app.listen(process.env.PORT, () => {
-  console.log(`app is started on server ${process.env.PORT}`)
-})
+const PORT = process.env.PORT || 8000
+const MONGOURl = process.env.MONGODB_URI
+
+mongoose
+  .connect(MONGOURl)
+  .then(() => {
+    console.log("mongodb connected succesfully!!")
+
+    app.listen(process.env.PORT, () => {
+      console.log(`app is started on server ${process.env.PORT}`)
+    })
+  })
+  .catch((error) => console.log(error))
